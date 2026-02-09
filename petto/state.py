478563215f68fa27311f4fa6943e7cfb9e43d6f5
zachary-chiodini <zacharybchiodini@gmail.com>
2026-02-09 00:24:29 +0000

    updated state

--- petto/state.py
+++ petto/state.py
@@ -6,36 +6,43 @@ class State:
     file = Path('petto/state.txt')
     friends = {}
 
-    def __init__(self):
-        self.age = 0
-        self.health = 0
-        self.hunger = 0
-        self.hygiene = 0
-        self.mood = 0
-        self.power = 0
-        self.stage = 0
-        self.thirst = 0
-        self.weight = 0
-        if self.file.exists():
-            with open(str(self.file)) as f:
-                line = f.readline()
-                if line:
-                    stats = line.split(',')
-                    for field, stat in zip(self.__dict__, stats):
-                        setattr(self, field, int(stat))
-                    for friend in f.readlines():
-                        friend_id, strength = map(int, friend.split(','))
-                        self.friends[friend_id] = strength
+    def __init__(self, age: int = 0, hunger: int = 0, hygiene: int = 0,
+            power: int = 0, stage: int = 0, thirst: int = 0, weight: int = 0,
+            overwrite_file: bool = False):
+        self.age = age
+        self.hunger = hunger
+        self.hygiene = hygiene
+        self.power = power
+        self.stage = stage
+        self.thirst = thirst
+        self.weight = weight
+        if overwrite_file:
+            self._save()
         else:
-            self.file.touch()
+            if self.file.exists():
+                with open(str(self.file)) as f:
+                    line = f.readline()
+                    if line:
+                        stats = line.split(',')
+                        for field, stat in zip(self.__dict__, stats):
+                            setattr(self, field, int(stat))
+                        for friend in f.readlines():
+                            friend_id, strength = map(int, friend.split(','))
+                            self.friends[friend_id] = strength
+            else:
+                self.file.touch()
 
     def format(self) -> str:
-        form = (f"{self.age:0>3},{self.health:0>3},{self.hunger:0>3},{self.hygiene:0>3},"
-            f"{self.mood:0>3},{self.power:0>3},{self.stage:0>2},{self.thirst:0>3},"
-            f"{self.weight:0>3}")
+        form = (f"{self.age:0>3},{self.hunger:0>3},{self.hygiene:0>3},"
+            f"{self.power:0>3},{self.stage:0>2},{self.thirst:0>3},{self.weight:0>3}")
         for friend_id, strength in self.friends.items():
             form = f"{form}\n{friend_id},{strength}"
         return f"{form}\n"
+
+    def update(self, field: str, n: int) -> None:
+        setattr(self, field, n)
+        self._save()
+        return None
 
     def _save(self) -> None:
         with open(str(self.file), 'w') as f:
